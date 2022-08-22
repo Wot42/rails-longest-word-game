c7f206b79fb3e524b7bbae51cf0015e19dddde80
Wot Fanar <wotfanar.webdeveloper@gmail.com>
2022-08-22 16:54:18 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'games#new'
-  post 'score', to: 'games#score'
-  # post 'answer', to: 'games#score'
 end
